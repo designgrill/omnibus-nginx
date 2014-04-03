739729fe8e94a45a9b8b0375e17c6dcfac4d9d43
Anshul Agrawal <anshul@mygola.com>
2014-04-03 23:29:51 +0530

    ngx_pagepseed is now url based instead of git

--- config/software/ngx_pagespeed.rb
+++ config/software/ngx_pagespeed.rb
@@ -1,12 +1,16 @@
 name "ngx_pagespeed"
-default_version "v1.7.30.4-beta"
+default_version "1.7.30.4-beta"
 
-source git: "https://github.com/pagespeed/ngx_pagespeed"
+source :url => "https://github.com/pagespeed/ngx_pagespeed/archive/release-#{version}.zip", :md5 => "d31ff96f44445b0c7d147d9c3eb8ae68"
 
 dependency "psol"
 
+relative_path "#{name}-release-#{version}"
+
 build do
-	FileUtils.cp_r(File.join(source_dir, "psol"),project_dir)
+	block do
+		FileUtils.cp_r(File.join(source_dir, "psol"),project_dir)
+	end
 end
 
 
--- config/software/psol.rb
+++ config/software/psol.rb
@@ -1,6 +1,6 @@
 name "psol"
 version "1.7.30.4"
 
-source :url => "https://dl.google.com/dl/page-speed/psol/#{version}.tar.gz"
+source :url => "https://dl.google.com/dl/page-speed/psol/#{version}.tar.gz", :md5 => "8ecb0af0c6e1bd62b6bdeee6c09a5e5b"
 
 relative_path = "psol"
